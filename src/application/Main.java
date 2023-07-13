81779735306f99eae86a148391d5b3e9d35315ce
akhilmanjesh <99862102+akhilmanjesh@users.noreply.github.com>
2023-07-13 16:34:51 -0700

    ic
    ic

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,42 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Skiver");
+
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(25, 25, 25, 25));
+
+        Text appName = new Text("Skiver");
+        Text appDescription = new Text("Secure Journaling Application");
+
+        PasswordField pwBox = new PasswordField();
+        pwBox.setPromptText("Password");
+
+        Button forgotPasswordButton = new Button("Forgot Password?");
+
+        vbox.getChildren().addAll(appName, appDescription, pwBox, forgotPasswordButton);
+
+        Scene scene = new Scene(vbox, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
